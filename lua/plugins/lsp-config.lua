0aee41f69990bb366965827ae515e4f38343e5ef
vini464 <vini.dev2003@gmail.com>
2024-07-21 13:36:57 -0300

    .

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "html-lsp", "css-lsp" },
+        ensure_installed = { "lua_ls", "tsserver", "html", "cssls" },
       })
     end,
   },
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,7 +6,7 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.glint,
+--				null_ls.builtins.diagnostics.glint, -- ins't working :/
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})

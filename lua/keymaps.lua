7e80bd26c0e1fe11513878e07310cd17ffa08010
vini464 <vini.dev2003@gmail.com>
2024-07-23 15:24:04 -0300

    feat(keymaps): resizing buffers

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -12,7 +12,14 @@ vim.keymap.set('n', '<leader>v', ':vsplit<cr>', {})
 
 -- movements:
 
-vim.keymap.set('n', '<C-j>', '<C-w>h', {})
-vim.keymap.set('n', '<C-i>', '<C-w>k', {})
-vim.keymap.set('n', '<C-k>', '<C-w>j', {})
-vim.keymap.set('n', '<C-l>', '<C-w>l', {})
+vim.keymap.set('n', '<C-Left>', '<C-w>h', {})
+vim.keymap.set('n', '<C-Up>', '<C-w>k', {})
+vim.keymap.set('n', '<C-Down>', '<C-w>j', {})
+vim.keymap.set('n', '<C-Right>', '<C-w>l', {})
+
+-- resizing
+
+vim.keymap.set('n', '<M-left>', ':vertical resize -2<cr>')
+vim.keymap.set('n', '<M-right>', ':vertical resize +2<cr>')
+vim.keymap.set('n', '<M-up>', ':horizontal resize -2<cr>')
+vim.keymap.set('n', '<M-down>', ':horizontal resize +2<cr>')

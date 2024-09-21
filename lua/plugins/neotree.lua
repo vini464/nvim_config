1e7c4c55e1e3ccce1846764b4ec62e7e9bfe1ddb
vini464 <vini.dev2003@gmail.com>
2024-09-21 14:06:22 -0300

    add Neotree flaot window keymap

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -2,10 +2,11 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   dependencies = {
-    "nvim-tree/nvim-web-devicons", 
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
   config = function()
     vim.keymap.set('n', '<C-n>', ':Neotree toggle left<CR>', {})
+    vim.keymap.set('n', '<leader>nn', ':Neotree toggle float<CR>', {})
   end
 }

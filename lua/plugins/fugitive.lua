5af9804aafefd53fad0e81b2f5ec498523943bda
vini464 <vini.dev2003@gmail.com>
2025-04-05 22:06:22 -0300

    fix(plugin): misspelling `amend` in fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,8 +1,8 @@
 return {
-  'tpope/vim-fugitive', 
+  'tpope/vim-fugitive',
   config = function ()
       vim.keymap.set("n", "<leader>ga", ":Git add -A<cr>", {})
       vim.keymap.set("n", "<leader>gc", ":Git commit<cr>", {})
-      vim.keymap.set("n", "<leader>gca", ":Git commit --ammend<cr>", {})
+      vim.keymap.set("n", "<leader>gca", ":Git commit --amend<cr>", {})
   end
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,14 @@
 return {
   'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+
   config = function()
     require('lualine').setup({
       options = {
-        theme = 'catppuccin-latte'
-      }})
+        theme = 'catppuccinlatte',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+      }
+    })
   end
 }
